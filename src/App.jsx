43a647cd22382421174a956485e59d6eee899b4c
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-07 22:43:34 +0500

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Button } from '@material-tailwind/react'
-import { useState } from 'react'
+import React from "react";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import ReactLayouts from "./layouts/ReactLayouts";
+import Hero from "./components/Hero";
+
 
 export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<ReactLayouts />}>
+        <Route path="/" element={<Hero />}/>
+      </Route>
+    )
+  );
+
   return (
     <div>
-      
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
