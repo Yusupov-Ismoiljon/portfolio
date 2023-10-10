18baa751ffe372ce09b1118de1f0e252e16d6940
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-10 17:30:49 +0500

    add grow

--- src/layouts/ReactLayouts.jsx
+++ src/layouts/ReactLayouts.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 const ReactLayouts = () => {
   return (
-    <div className='w-ful h-screen bg-black'>
+    <div className='flex flex-col w-full h-screen dark:bg-black bg-white'>
       <Header />
-      <main>
+      <main className='grow'>
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
