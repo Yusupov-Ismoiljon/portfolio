bf69a9e5194a6565d09f6e0b6556e4c186dfe343
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-13 16:17:32 +0500

    I added a menu to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import ismoiljon from '../img/ismoiljon.jpg';
 import tun from '../img/tun.png';
 import kun from '../img/day.png';
 
@@ -8,6 +7,7 @@ import kun from '../img/day.png';
 const Header = () => {
   const darkMode = localStorage.getItem('dark');
   const [dark, setDark] = useState(darkMode || 'light');
+  const [menu, setMenu] = useState(false)
 
   useEffect(() => {
     localStorage.setItem("dark", dark);
@@ -21,14 +21,14 @@ const Header = () => {
 
   const addDark = () => {
     setDark(dark === 'dark' ? 'light' : 'dark');
-  }
+  };
   return (
-    <div id='uy' className='pt-10 pb-20'>
+    <div id='uy' className='py-20'>
       <div className='w-full max-w-[1440px] mx-auto px-5'>
         <div className='flex justify-between items-center'>
           <Link className='w-[100px] h-[100px] rounded-full bg-white' to={'/'}>
           </Link>
-          <nav>
+          <nav className='md:inline-block hidden'>
             <ul className='flex items-center gap-10'>
               <li>
                 <a className='font-medium text-xl nav-link-color dark:text-white leading-6 hover dark-hover duration-300' href="#uy">Uy</a>
@@ -44,12 +44,40 @@ const Header = () => {
               </li>
             </ul>
           </nav>
-          <div className='flex items-center gap-5'>
+          <div className='flex items-center dark:text-white gap-5'>
             <a target='_blank' href='https://github.com/iyiy2008' className='inline-block text-black  dark:text-white'><i className="fa-brands fa-github fa-2xl"></i></a>
             <a target='_blank' href='https://t.me/Ismoiljon_12_04' className='inline-block text-black dark:text-white'><i className="fa-brands fa-telegram fa-2xl"></i></a>
             <button onClick={addDark}>
               <img className='w-8 h-8' src={dark === 'light' ? kun : tun} alt="" />
             </button>
+            <button onClick={() => setMenu(!menu)}>
+              <i className="fa-solid fa-bars md:hidden inline-block fa-xl"></i>
+            </button>
+            <div className='md:hidden block'>
+              {menu &&
+                <nav className='fixed w-full h-full top-0 right-0 bg-white dark:bg-black'>
+                  <div className='relative'>
+                    <button className='absolute top-9  right-5' onClick={() => setMenu(!menu)}>
+                      <i class="fa-solid fa-xmark fa-2xl" style={{ 'color': '#ff0000' }}></i>
+                    </button>
+                    <ul onClick={()=> setMenu(!menu)} className='flex flex-col gap-10 absolute left-10 top-14'>
+                      <li>
+                        <a className='font-medium text-xl nav-link-color dark:text-white leading-6 hover dark-hover duration-300' href="#uy">Uy</a>
+                      </li>
+                      <li>
+                        <a className='font-medium text-xl nav-link-color dark:text-white leading-6 hover dark-hover duration-300' href="#ha">Haqida</a>
+                      </li>
+                      <li>
+                        <a className='font-medium text-xl nav-link-color dark:text-white leading-6 hover dark-hover duration-300' href="#te">Texnologa</a>
+                      </li>
+                      <li>
+                        <a className='font-medium text-xl nav-link-color dark:text-white leading-6 hover dark-hover duration-300' href="#lo">Loyihalar</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              }
+            </div>
           </div>
         </div>
       </div>
