303b8b076e9258666aa9c0a17bef159386707c95
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-17 13:27:10 +0500

    I added responsive to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 const Hero = () => {
   return (
     <div id='ha' className='w-full max-w-[1440px] mx-auto px-5 py-20'>
-      <div className='flex items-center justify-between'>
-        <h2 className='font-bold text-5xl leading-[60px] text-[#42446E] dark:text-white'>Salom 👋,<br />
-          Mening ismim <br />
-          <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#00C0FD] to-[#E70FAA]'>
-            Yusupov Ismoiljon
-          </span><br />
-          Men veb-sayt yarataman
-        </h2>
-        <div className='flex justify-center text-center items-center w-[300px] h-[300px] bg-gradient-to-br to-[#E70FAA] from-[#00c0fd] dark:bg-gradient-to-br dark:from-[#E70FAA] dark:to-[#00c0fd] bg-clip-content p-6 rounded-full'>
-          <div className='w-[240px] h-[240px] rounded-full bg-black'>
+      <div className='hero-responsive'>
+        <div className='lg:hidden block mb-10'>
+          <div className='flex justify-center text-center items-center w-[270px] h-[270px] bg-gradient-to-br to-[#E70FAA] from-[#00c0fd] dark:bg-gradient-to-br dark:from-[#E70FAA] dark:to-[#00c0fd] bg-clip-content p-6 rounded-full'>
+            <div className='w-[210px] h-[210px] rounded-full bg-black'>
 
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className='font-bold text-4xl lg:text-5xl leading-[60px] text-[#42446E] dark:text-white'>Salom 👋,<br />
+            Mening ismim <br />
+            <span className='hero-title'>
+              <Typewriter
+                options={{
+                  autoStart: true,
+                  loop: true,
+                  className:"hero-title"  ,
+                }}
+                onInit={(typewriter) => {
+                  typewriter.typeString('Yusupov Ismoiljon')
+                    .pauseFor(0)
+                    .deleteAll(100)
+                    .start(0);
+                }}
+              />
+              
+            </span>
+            Men veb-sayt yarataman
+          </h2>
+
+        </div>
+        <div className='lg:block hidden'>
+          <div className='flex justify-center text-center items-center w-[300px] h-[300px] bg-gradient-to-br to-[#E70FAA] from-[#00c0fd] dark:bg-gradient-to-br dark:from-[#E70FAA] dark:to-[#00c0fd] bg-clip-content p-6 rounded-full'>
+            <div className='w-[240px] h-[240px] rounded-full bg-black'>
+
+            </div>
           </div>
         </div>
       </div>
