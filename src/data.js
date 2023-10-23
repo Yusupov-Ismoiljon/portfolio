5b013a307570485afac9c138925a84a56b8992f8
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-23 13:47:29 +0500

    I deleted the data

--- src/data.js
+++ src/data.js
@@ -11,22 +11,4 @@ export const loyihalar = [
         sahifasoni: 6,
         texnalogiya: 'Vite React, Tailwind Css, Material Tailwind React, React Router Dom'
     },
-    {
-        id: 2,
-        img: apteka,
-        title: 'MyTeam',
-        text: `Ushbu sayt 'Utrujja team' jamoasi haqida ma'lumot beradi.Sayt Css amaliyotda qo'llash va yaxshiroq o'rganish maqsadida test loyiha sifatida qilingan.`,
-        vaqti: '27-fevral 2023-yil',
-        sahifasoni: 1,
-        texnalogiya: 'HTML, CSS'
-    },
-    {
-        id: 3,
-        img: myteamimg,
-        title: 'MyTeam',
-        text: `Ushbu sayt 'Utrujja team' jamoasi haqida ma'lumot beradi.Sayt Css amaliyotda qo'llash va yaxshiroq o'rganish maqsadida test loyiha sifatida qilingan.`,
-        vaqti: '27-fevral 2023-yil',
-        sahifasoni: 3,
-        texnalogiya: 'HTML, CSS'
-    },
 ]
