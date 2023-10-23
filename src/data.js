524fa4315d7d6820d0b725d267c05fd27bc05f6a
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-23 13:44:54 +0500

    I changed data

--- src/data.js
+++ src/data.js
@@ -8,8 +8,8 @@ export const loyihalar = [
         title: 'Akstar',
         text: `Ushbu sayt React amaliyotda qo'llash va yaxshiroq o'rganish maqsadida test loyiha sifatida qilingan.`,
         vaqti: '30-sentabr 2023-yil',
-        sahifasoni: 3,
-        texnalogiya: 'HTML, CSS'
+        sahifasoni: 6,
+        texnalogiya: 'Vite React, Tailwind Css, Material Tailwind React, React Router Dom'
     },
     {
         id: 2,
